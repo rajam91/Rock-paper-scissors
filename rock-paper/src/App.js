d6486eb62b306da3cb79037acf2e4b6077283026
raja <marwah@MacBook-Air-de-Marwah.local>
2025-03-14 12:04:24 +0100

    react project

---
+++ rock-paper/src/App.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import '../src/App.css';
+import Game from './components/Game';
+
+class App extends Component{  
+    render(){
+      return (
+      <Game/>
+    );
+  }
+}
+
+export default App;
